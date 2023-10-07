70b528d45ef17b98d014206aba552a98d858e1bb
MoustafaObari <moustafaobari@gmail.com>
2023-10-07 17:56:29 -0400

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//app.js_Mercy_Adebayo_301246292_1/10/2023-->
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -11,7 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,22 +21,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+res.status(err.status || 500);
+res.render('error');
 });
 
-
+app.listen(3000,function(req,res){ 
+console.log('Server started at 3000') 
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
@@ -21,11 +20,8 @@ res.render('contact', { title: 'Contact' });
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services' });
 });
-/*index.js_Mercy_Adebayo_301246292_1/10/2023*/
-// Handle form submission
 router.post('/contact', (req, res) => {
-  // Extract form data from req.body
-  const { firstName, lastName, contactNumber, email, message } = req.body;
+const { firstName, lastName, contactNumber, email, message } = req.body;
 
   console.log('Received Contact Form Submission:');
   console.log('First Name:', firstName);
@@ -34,7 +30,6 @@ router.post('/contact', (req, res) => {
   console.log('Email:', email);
   console.log('Message:', message);
 
-  // Redirect back to the Home Page
   res.redirect('/');
 });
 module.exports = router;
